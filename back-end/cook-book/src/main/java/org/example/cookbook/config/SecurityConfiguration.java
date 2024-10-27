36a7588c16aa8b68d94299a9cb72917d0c5c5c0f
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-10-27 23:54:50 +0200

    adding security

--- back-end/cook-book/src/main/java/org/example/cookbook/config/SecurityConfiguration.java
+++ back-end/cook-book/src/main/java/org/example/cookbook/config/SecurityConfiguration.java
@@ -2,11 +2,29 @@ package org.example.cookbook.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfiguration {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request.requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/recipe/**").permitAll()
+                        .requestMatchers("/api/recipe/**").authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(FormLoginConfigurer::disable)
+                .csrf(CsrfConfigurer::disable);
+
+        return http.build();
+    }
+
     @Bean
     WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
