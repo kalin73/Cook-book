6de1f1641cccbf3e6e485df333836a4a38cd9015
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-10-28 16:39:29 +0200

    refactoring

--- back-end/cook-book/src/main/java/org/example/cookbook/security/SecurityConfiguration.java
+++ back-end/cook-book/src/main/java/org/example/cookbook/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
-package org.example.cookbook.config;
+package org.example.cookbook.security;
 
+import lombok.RequiredArgsConstructor;
 import org.example.cookbook.service.AppUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +14,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request.requestMatchers("/api/auth/**").permitAll()
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/recipe/**").permitAll()
                         .requestMatchers("/api/recipe/**").authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
