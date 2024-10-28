713fabf43c6b0cbc7959a74f0a8e8071ceb43e4a
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-10-28 16:39:41 +0200

    filter

---
+++ back-end/cook-book/src/main/java/org/example/cookbook/security/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package org.example.cookbook.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.cookbook.service.JwtService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            UserDetails user = jwtService.getUserDetails(token);
+
+            if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
