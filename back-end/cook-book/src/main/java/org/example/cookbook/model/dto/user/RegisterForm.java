da98485865497e6b7459001ed38c0d8e6b2ac214
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-11-04 23:02:32 +0200

    validation with regex

--- back-end/cook-book/src/main/java/org/example/cookbook/model/dto/user/RegisterForm.java
+++ back-end/cook-book/src/main/java/org/example/cookbook/model/dto/user/RegisterForm.java
@@ -1,6 +1,7 @@
 package org.example.cookbook.model.dto.user;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,15 +13,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterForm {
-    @Size(min = 3, max = 30)
+    @Pattern(regexp = "[a-z]{3,30}", message = "should be between 3 and 30 letters!")
     private String firstName;
 
-    @Size(min = 3, max = 30)
+    @Pattern(regexp = "[a-z]{3,30}", message = "should be between 3 and 30 letters!")
     private String lastName;
 
     @Email
     private String email;
 
-    @Size(min = 3, max = 30)
+    @Size(min = 3, max = 30, message = "should be between 3 and 30 symbols!")
     private String password;
 }
