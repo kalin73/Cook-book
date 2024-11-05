bb15f43f7f10f74a578cc11e69ed1cbd31ffd484
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-11-06 01:04:56 +0200

    refactoring

--- back-end/flyway/src/main/resources/db/migrations/V1__create_users_recipes_ingredients_tables.sql
+++ back-end/flyway/src/main/resources/db/migrations/V1__create_users_recipes_ingredients_tables.sql
@@ -1,3 +1,13 @@
+CREATE TABLE users
+(
+    id         VARCHAR(255) PRIMARY KEY,
+    email      VARCHAR(55) UNIQUE NOT NULL,
+    first_name VARCHAR(55),
+    last_name  VARCHAR(55),
+    password   VARCHAR(255)       NOT NULL,
+    created_at TIMESTAMP(6)
+);
+
 CREATE TABLE recipes
 (
     id          VARCHAR(255) PRIMARY KEY,
--- back-end/flyway/src/main/resources/db/migrations/V1__create_users_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users
-(
-    id         VARCHAR(255) PRIMARY KEY,
-    email      VARCHAR(55) UNIQUE NOT NULL,
-    first_name VARCHAR(55),
-    last_name  VARCHAR(55),
-    password   VARCHAR(255)       NOT NULL,
-    created_at TIMESTAMP(6)
-);
