74515b1c38eb7006fbe7c1167ecd3eec75e4b23e
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-11-06 00:56:05 +0200

    refactoring

--- back-end/flyway/src/main/resources/db/migrations/V1__create_users_tables.sql
+++ back-end/flyway/src/main/resources/db/migrations/V1__create_users_tables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users
 (
-    id         SERIAL PRIMARY KEY ,
+    id         VARCHAR(255) PRIMARY KEY,
+    email      VARCHAR(55) UNIQUE NOT NULL,
     first_name VARCHAR(55),
     last_name  VARCHAR(55),
-    email      VARCHAR(55),
-    password   VARCHAR(255) NOT NULL,
+    password   VARCHAR(255)       NOT NULL,
     created_at TIMESTAMP(6)
 );
--- back-end/flyway/src/main/resources/db/migrations/V2__create_recipes_and_incredients_tables.sql
+++ back-end/flyway/src/main/resources/db/migrations/V2__create_recipes_and_incredients_tables.sql
@@ -1,18 +1,22 @@
 CREATE TABLE recipes
 (
-    id          SERIAL PRIMARY KEY,
-    user_id     BIGINT,
+    id          VARCHAR(255) PRIMARY KEY,
+    user_id     VARCHAR(255),
     title       VARCHAR(55) NOT NULL,
     preparation TEXT,
     image_url   VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    created_at  TIMESTAMP(6),
+    CONSTRAINT pk_users_id
+        FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE ingredients
 (
-    id        SERIAL PRIMARY KEY,
-    recipe_id BIGINT,
+    id        VARCHAR(255) PRIMARY KEY,
+    recipe_id VARCHAR(255),
     name      VARCHAR(55) NOT NULL,
     quantity  VARCHAR(55) NOT NULL,
-    FOREIGN KEY (recipe_id) REFERENCES recipes (id)
+    CONSTRAINT fk_recipes_id
+        FOREIGN KEY (recipe_id) REFERENCES recipes (id)
+
 );
