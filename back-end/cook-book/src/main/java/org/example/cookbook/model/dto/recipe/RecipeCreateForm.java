594afc597bba9f3ff2936b02c32351161dd5cab5
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-12-06 22:59:18 +0200

    adding constraints

--- back-end/cook-book/src/main/java/org/example/cookbook/model/dto/recipe/RecipeCreateForm.java
+++ back-end/cook-book/src/main/java/org/example/cookbook/model/dto/recipe/RecipeCreateForm.java
@@ -1,5 +1,7 @@
 package org.example.cookbook.model.dto.recipe;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,12 +10,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RecipeCreateForm {
+    @Size(min = 1, max = 50)
     private String title;
 
+    @NotBlank
     private String preparation;
 
+    @NotBlank
     private String imageUrl;
 
+    @NotBlank
     private String ingredients;
 
     private String userId;
