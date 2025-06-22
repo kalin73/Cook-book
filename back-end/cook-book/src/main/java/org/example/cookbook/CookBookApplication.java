3f461224aee814afcb856c793c7a11b65ff579dc
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-06-22 17:08:35 +0300

    adding openapi info

--- back-end/cook-book/src/main/java/org/example/cookbook/CookBookApplication.java
+++ back-end/cook-book/src/main/java/org/example/cookbook/CookBookApplication.java
@@ -1,5 +1,7 @@
 package org.example.cookbook;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @SpringBootApplication
 @EnableCaching
 @EnableMethodSecurity
+@OpenAPIDefinition(info = @Info(title = "Cook Book API", version = "1.0", description = "Cook Book Microservice"))
 public class CookBookApplication {
 
     public static void main(String[] args) {
